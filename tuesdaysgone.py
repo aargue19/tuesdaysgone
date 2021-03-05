7249f6ede33449e04bbf11baf07948c47750132c
Ande Gao <aargue16@gmail.com>
2021-03-05 09:54:26 +0800

    fixed next button and higlighting in right window

--- tuesdaysgone.py
+++ tuesdaysgone.py
@@ -101,7 +101,11 @@ def destroy_info():
     idInfo.destroy()
     gameDescInfo.destroy()
     gameDescTxt.delete('1.0', tk.END)
-    backMatchBox.delete('1.0', tk.END)
+    #backMatchBox.delete('1.0', tk.END)
+
+    searchCanvas.destroy()
+    searchListBox.destroy()
+
 
 def update_info():
     global numInfo
@@ -111,6 +115,12 @@ def update_info():
     global current_game_df
     global hoverList
     global matchCase
+    global std_list
+    global std_spacing
+    global labelList
+    global rmk_spacing
+    global remark_list
+    global hoverList
 
     numInfo = Label(frame1, text=df.iloc[currentIndex].game_num)
     numInfo.place(x=10, y=35)
@@ -121,7 +131,11 @@ def update_info():
     gameDescInfo = Label(frame1, text=df.iloc[currentIndex].game_name)
     gameDescInfo.place(x=300, y=35)
     gameDescInfo.configure(background = "black", foreground="white", font=('Consolas', 12))
-    description = str(df.iloc[currentIndex].game_description).replace(" ","_").lower()
+    description = str(df.iloc[currentIndex].game_description).replace("_"," ").lower()
+    description = description.replace("."," ")
+    description = description.replace('"',' ')
+    description = description.replace('!',' ')
+    description = description.replace(',',' ')
     gameDescTxt.insert(1.0, description, 'warning')
 
     currentGameId = df.iloc[currentIndex].id
@@ -130,17 +144,17 @@ def update_info():
 
     current_game_df = current_game_df.sort_values("index")
 
-    test_desc = str(df.iloc[currentIndex].game_description).replace(" ","_").lower()
-    test_desc = test_desc.replace(".","_")
-    test_desc = test_desc.replace('"','_')
-    test_desc = test_desc.replace('!','_')
-    test_desc = test_desc.replace(',','_')
+    test_desc = str(df.iloc[currentIndex].game_description).replace("_"," ").lower()
+    test_desc = test_desc.replace("."," ")
+    test_desc = test_desc.replace('"',' ')
+    test_desc = test_desc.replace('!',' ')
+    test_desc = test_desc.replace(',',' ')
     #print(test_desc)
 
-    desc_word_list = test_desc.split("_")
+    desc_word_list = test_desc.split(" ")
     #print(desc_word_list)
 
-    backMatchBox.insert(1.0, "{}".format(current_game_df))
+    # backMatchBox.insert(1.0, "{}".format(current_game_df))
 
     matchDf = current_game_df.loc[current_game_df['id'] == currentGameId]
     matchDf = (current_game_df[['id','word','index','std_word', 'remark', 'duplicate']]) 
@@ -149,70 +163,126 @@ def update_info():
     werds = []
 
     for werd in matchDf['word']:
-        lower_werd = werd.lower().split("_")[0]
-        werd_order.append(desc_word_list.index(lower_werd))
-        werds.append(lower_werd)
+        werd = werd.replace("."," ")
+        werd = werd.replace('"',' ')
+        werd = werd.replace('!',' ')
+        werd = werd.replace(',',' ')
+        lower_werd = werd.lower().split(" ")[0]
 
-    print(werds)
-    print(werd_order)
+        if lower_werd in desc_word_list:
+            werd_order.append(desc_word_list.index(lower_werd))
+            werds.append(lower_werd)
+        else:
+            werds.append("na")
+            werd_order.append(99)
+        
     
-
-
     matchDf['order'] = werd_order
     matchDf = matchDf.sort_values('order')
-
-    print(matchDf)
-
     matchList = []
+
+    searchCanvas = Canvas(frame2, bg='black', width=350, height=900)
+    searchCanvas.place(x=5,y=60)
+    searchListBox = st.ScrolledText(searchCanvas, width=140, height=52, wrap="none")
+    searchListBox.configure(background = "black")
+    searchListBox.pack() 
 
     for i in range(len(matchDf)):
         # matchList.append(matchDf[i])
         matchList = matchDf.values.tolist()
-    checkBoxList = []
-    checkBoxes = []
+
     labelList = []
     hoverList = []
+    std_list = []
+    std_spacing = []
+    rmk_spacing = []
+    remark_list = []
     for matchCase in range(len(matchList)):
-        labelList.append(Label(searchListBox, text=matchList[matchCase][1]))
-        labelList[matchCase].changed_word = matchList[matchCase]
+        labelList.append(Label(searchListBox, text=matchList[matchCase][1].replace("_"," ")))
+        # labelList[matchCase].changed_word = matchList[matchCase]
         labelList[matchCase].config(background = "black", foreground= 'white', font = ('Consolas', 12, 'bold'))
-        # labelList[i].bind("<Enter>", on_enter)
-        # labelList[i].bind("<Leave>", on_leave)
-        checkBoxList.append(IntVar())
-        checkBoxes.append(Checkbutton(searchListBox, text='', variable=checkBoxList[matchCase], selectcolor="grey88", background='black'))
+        labelList[matchCase].bind("<Enter>", on_enter)
+        labelList[matchCase].bind("<Leave>", on_leave)
+        labelList[matchCase].word = matchList[matchCase]
+        labelList[matchCase].number = matchCase
+
         hoverList.append(Label(searchListBox, text="HOVER OVER TO SEE DESCRIPTIONS   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "))
         hoverList[matchCase].word = matchList[matchCase]
+        hoverList[matchCase].number = matchCase
         hoverList[matchCase].config(background = "black", foreground= 'black', font = ('Consolas', 12))
         hoverList[matchCase].bind("<Enter>", on_enter)
         hoverList[matchCase].bind("<Leave>", on_leave)
-        searchListBox.window_create("end", window=checkBoxes[matchCase])
+        
+        std_space_len = 40 - len(matchList[matchCase][1])
+        rmk_space_len = 40 - len(matchList[matchCase][3])
+
+        std_spacing.append(Label(searchListBox, text= "x" * std_space_len))
+        std_spacing[matchCase].config(background = "black", foreground= 'black', font = ('Consolas', 12, 'bold'))
+        std_spacing[matchCase].bind("<Enter>", on_enter)
+        std_spacing[matchCase].bind("<Leave>", on_leave)
+        std_spacing[matchCase].word = matchList[matchCase]
+        std_spacing[matchCase].number = matchCase
+
+        std_list.append(Label(searchListBox, text=matchList[matchCase][3]))
+        std_list[matchCase].config(background = "black", foreground= 'white', font = ('Consolas', 12, 'bold'))
+        std_list[matchCase].bind("<Enter>", on_enter)
+        std_list[matchCase].bind("<Leave>", on_leave)
+        std_list[matchCase].word = matchList[matchCase]
+        std_list[matchCase].number = matchCase
+
+        rmk_spacing.append(Label(searchListBox, text= "x" * rmk_space_len))
+        rmk_spacing[matchCase].config(background = "black", foreground= 'black', font = ('Consolas', 12, 'bold'))
+        rmk_spacing[matchCase].bind("<Enter>", on_enter)
+        rmk_spacing[matchCase].bind("<Leave>", on_leave)
+        rmk_spacing[matchCase].word = matchList[matchCase]
+        rmk_spacing[matchCase].number = matchCase
+
+        remark_list.append(Label(searchListBox, text=matchList[matchCase][4]))
+        remark_list[matchCase].config(background = "black", foreground= 'white', font = ('Consolas', 12, 'bold'))
+        remark_list[matchCase].bind("<Enter>", on_enter)
+        remark_list[matchCase].bind("<Leave>", on_leave)
+        remark_list[matchCase].word = matchList[matchCase]
+        remark_list[matchCase].number = matchCase
+
+
         searchListBox.window_create("end", window=labelList[matchCase])
+
+        searchListBox.window_create("end", window=std_spacing[matchCase])     
+
+        searchListBox.window_create("end", window=std_list[matchCase])
+
+        searchListBox.window_create("end", window=rmk_spacing[matchCase]) 
+
+        searchListBox.window_create("end", window=remark_list[matchCase]) 
+
         searchListBox.window_create("end", window=hoverList[matchCase])
         searchListBox.insert("end", "\n")
+
+
 
     #highlight_word()
 
 def on_enter(event):
     global hoverList
     global matchCase
-    w = getattr(event.widget, "word", "")[1].lower().replace(" ","_")
+    w = getattr(event.widget, "word", "")[1].lower().replace("_"," ")
     global start_pos
     start_pos = 0
     global end_pos
     end_pos = ''
-    #gameDescTxt.delete('1.0', tk.END)
-    #gameDescTxt.insert(1.0, df.iloc[currentIndex].game_description, 'warning')
-    #h_word = df.iloc[currentIndex].word.replace(" ","_")
-    # h_word = w.replace(" ","_")
+    global std_list
+    global std_spacing
+    global labelList
+    global rmk_spacing
+    global remark_list
+    global hoverList
+
+    num_selected = getattr(event.widget, "number", "")
+
     h_word = w.strip()
     
     start_pos = gameDescTxt.search(h_word, '1.0', stopindex=tk.END)
-    # if not start_pos:   #in case the word is capitalized b/c it's first word in sentence
-    #     start_pos = gameDescTxt.search(h_word.capitalize(), '1.0', stopindex=tk.END)
-    # if not start_pos:   #in case the word is all uppercase letters  
-    #     start_pos = gameDescTxt.search(h_word.upper(), '1.0', stopindex=tk.END)
-    # if not start_pos:   #in case the word has hyphens
-    #     start_pos = gameDescTxt.search(h_word.replace(" ","-").lower(), '1.0', stopindex=tk.END)
+
     if start_pos:
         if end_pos:
             gameDescTxt.tag_remove('highlight', start_pos, end_pos)
@@ -220,37 +290,30 @@ def on_enter(event):
         gameDescTxt.tag_add('highlight', start_pos, end_pos)
         gameDescTxt.tag_config('highlight', background='yellow', foreground = "black")
 
+    std_list[num_selected].config(background = "yellow", foreground= 'black', font = ('Consolas', 12, 'bold'))
+    std_spacing[num_selected].config(background = "yellow", foreground= 'yellow', font = ('Consolas', 12, 'bold'))
+    labelList[num_selected].config(background = "yellow", foreground= 'black', font = ('Consolas', 12, 'bold'))
+    rmk_spacing[num_selected].config(background = "yellow", foreground= 'yellow', font = ('Consolas', 12, 'bold'))
+    remark_list[num_selected].config(background = "yellow", foreground= 'black', font = ('Consolas', 12, 'bold'))
+    hoverList[num_selected].config(background = "yellow", foreground= 'yellow', font = ('Consolas', 12, 'bold'))
+
 def on_leave(event):
+    global std_list
+    global std_spacing
+    global labelList
+    global rmk_spacing
+    global remark_list
+    global hoverList
+
+    num_selected = getattr(event.widget, "number", "")
+    std_list[num_selected].config(background = "black", foreground= 'white', font = ('Consolas', 12, 'bold'))
+    std_spacing[num_selected].config(background = "black", foreground= 'black', font = ('Consolas', 12, 'bold'))
+    labelList[num_selected].config(background = "black", foreground= 'white', font = ('Consolas', 12, 'bold'))
+    std_spacing[num_selected].config(background = "black", foreground= 'black', font = ('Consolas', 12, 'bold'))
+    rmk_spacing[num_selected].config(background = "black", foreground= 'black', font = ('Consolas', 12, 'bold'))
+    remark_list[num_selected].config(background = "black", foreground= 'white', font = ('Consolas', 12, 'bold'))
+    hoverList[num_selected].config(background = "black", foreground= 'black', font = ('Consolas', 12, 'bold'))    
     gameDescTxt.tag_remove('highlight', '1.0', tk.END)
-
-# def highlight_word():
-
-#     currentGameWordList = current_game_df['word']
-#     for w in currentGameWordList:
-#         print(w.replace("_"," "))
-
-#         global start_pos
-#         start_pos = 0
-#         global end_pos
-#         end_pos = ''
-#         #gameDescTxt.delete('1.0', tk.END)
-#         #gameDescTxt.insert(1.0, df.iloc[currentIndex].game_description, 'warning')
-#         #h_word = df.iloc[currentIndex].word.replace(" ","_")
-#         h_word = w.replace(" ","_")
-#         h_word = w.strip()
-#         start_pos = gameDescTxt.search(h_word, '1.0', stopindex=tk.END)
-#         if not start_pos:   #in case the word is capitalized b/c it's first word in sentence
-#             start_pos = gameDescTxt.search(h_word.capitalize(), '1.0', stopindex=tk.END)
-#         if not start_pos:   #in case the word is all uppercase letters  
-#             start_pos = gameDescTxt.search(h_word.upper(), '1.0', stopindex=tk.END)
-#         if not start_pos:   #in case the word has hyphens
-#             start_pos = gameDescTxt.search(h_word.replace(" ","-"), '1.0', stopindex=tk.END)
-#         if start_pos:
-#             if end_pos:
-#                 gameDescTxt.tag_remove('highlight', start_pos, end_pos)
-#             end_pos = '{}+{}c'.format(start_pos, len(h_word))            
-#             gameDescTxt.tag_add('highlight', start_pos, end_pos)
-#             gameDescTxt.tag_config('highlight', background='yellow', foreground = "black")
 
 def load_file():
     global df
@@ -337,17 +400,29 @@ gameDescTxt.insert(1.0, "LOAD A FILE..")
 
 #FRAME 2
 
+word_label = Label(frame2, text="WORD")
+word_label.place(x=5,y=5)
+word_label.configure(background = "black", foreground="white", font=('Consolas', 12))
+
+std_label = Label(frame2, text="STD WORD")
+std_label.place(x=370,y=5)
+std_label.configure(background = "black", foreground="white", font=('Consolas', 12))
+
+rmk_label = Label(frame2, text="REMARK")
+rmk_label.place(x=760,y=5)
+rmk_label.configure(background = "black", foreground="white", font=('Consolas', 12))
+
 searchCanvas = Canvas(frame2, bg='black', width=350, height=900)
-searchCanvas.place(x=5,y=5)
-searchListBox = st.ScrolledText(searchCanvas, width=40, height=52, wrap="none")
+searchCanvas.place(x=5,y=60)
+searchListBox = st.ScrolledText(searchCanvas, width=140, height=52, wrap="none")
 searchListBox.configure(background = "black")
 searchListBox.pack() 
 
-backMatchCanvas = Canvas(frame2, bg='black', width=800, height=900)
-backMatchCanvas.place(x=350,y=5)
-backMatchBox = st.ScrolledText(backMatchCanvas, width=120, height=46, wrap="none")
-backMatchBox.configure(background = "black", foreground="white", font=('Consolas', 12))
-backMatchBox.place(x=5,y=5)
+# backMatchCanvas = Canvas(frame2, bg='black', width=800, height=900)
+# backMatchCanvas.place(x=350,y=5)
+# backMatchBox = st.ScrolledText(backMatchCanvas, width=120, height=46, wrap="none")
+# backMatchBox.configure(background = "black", foreground="white", font=('Consolas', 12))
+# backMatchBox.place(x=5,y=5)
 
 # MAIN LOOP
 root.mainloop()
